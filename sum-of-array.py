88ab8208fd3b33243f48382c4bd9370fc1c17403
Sieva-cmd <musyokasieva@gmail.com>
2022-06-15 22:51:55 +0300

    creating a function to add integers in an array

---
+++ sum-of-array.py
@@ -0,0 +1,11 @@
+ar =[1,2,3,4,5,6]
+def simpleArraySum(ar):
+    sum =0
+    for i in range(0,len(ar)):
+        sum = sum + ar[i]
+        print(sum)
+    return sum 
+
+     
+simpleArraySum(ar)       
+       
---
+++ sum-of-intergers.js
@@ -0,0 +1,10 @@
+arr =[2,3,4,5,6,7]
+function sumOfIntergers(arr) {
+    sum =0;
+    for (i=0;i< length(arr);i++){
+        sum =sum + arr[i]
+        console.log(sum)
+
+    }
+    
+} sumOfIntergers(arr)
