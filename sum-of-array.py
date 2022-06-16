9655388de3bd3232e2499e815bc926dfe01ff4c8
Sieva-cmd <musyokasieva@gmail.com>
2022-06-16 12:24:33 +0300

    write the pseudocode

--- sum-of-array.py
+++ sum-of-array.py
@@ -1,3 +1,13 @@
+# pseudocode
+# write a function to sum intergers in an array
+# declare an array of integers
+# write a function that takes the array as the argument
+# initialize a variable sum to zero
+# loop through the array ,from the first index at 0 to the last index 
+# add the integers of the array at certain index to the initial sum variable and store the anser in another variable
+# return the sum
+
+# code
 ar =[1,2,3,4,5,6]
 def simpleArraySum(ar):
     sum =0
